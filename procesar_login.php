d2ce3942c64d7d88a69b89076f74556149e5a6bd
Josue Ayala <jayalaa1900@alumno.ipn.mx>
2024-02-26 22:53:58 -0600

    Modificación del archivo procesar_login: Cambio de funciones de mysql a postgresql

--- procesar_login.php
+++ procesar_login.php
@@ -1,38 +1,43 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$clave = "";
-$baseDeDatos = "loginep";
-$enlace = mysqli_connect($servidor, $usuario, $clave, $baseDeDatos);
+// Datos de conexión a la base de datos
+$servidor = "ec2-52-54-200-216.compute-1.amazonaws.com";
+$usuario = "rzcndrfatvphqy";
+$clave = "1c11fd7412c615db1fa8bc7dd5d5353650f3383ca6f549ee6cf92514cf392ab0";
+$baseDeDatos = "d2em42nge4v4em";
 
-if (!$enlace) {
-    die("Error al conectar: " . mysqli_connect_error());
+// Cadena de conexión
+$dsn = "pgsql:host=$servidor;dbname=$baseDeDatos";
+
+// Intentar establecer la conexión con la base de datos
+try {
+    $enlace = new PDO($dsn, $usuario, $clave);
+    $enlace->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error al conectar: " . $e->getMessage());
 }
 
-if(isset($_POST["login"])){
+if (isset($_POST["login"])) {
     $correo = $_POST['correo'];
     $contraseña = $_POST['contraseña'];
 
     // Consulta SQL para obtener el usuario por correo electrónico
-    $consultaUsuario = "SELECT * FROM Login WHERE correo = '$correo'";
-    $resultadoConsulta = mysqli_query($enlace, $consultaUsuario);
+    // Uso de consultas preparadas para evitar inyección SQL
+    $consultaUsuario = $enlace->prepare("SELECT * FROM Login WHERE correo = :correo");
+    $consultaUsuario->bindParam(':correo', $correo, PDO::PARAM_STR);
+    $consultaUsuario->execute();
 
-    if(mysqli_num_rows($resultadoConsulta) == 1) {
+    if ($consultaUsuario->rowCount() > 0) {
         // Usuario encontrado, verificar la contraseña
-        $usuario = mysqli_fetch_assoc($resultadoConsulta);
+        $usuario = $consultaUsuario->fetch(PDO::FETCH_ASSOC);
         echo "Contraseña almacenada en la base de datos: " . $usuario['contraseña'] . "<br>";
 
-        // Imprimir la longitud de ambas contraseñas para verificar si son iguales
-        echo "Longitud de la contraseña almacenada: " . strlen($usuario['contraseña']) . "<br>";
-        echo "Longitud de la contraseña proporcionada: " . strlen($contraseña) . "<br>";
-
         // Comparar contraseñas
-        if(password_verify($contraseña, $usuario['contraseña'])) {
+        if (password_verify($contraseña, $usuario['contraseña'])) {
             // Contraseña correcta
             session_start();
             $_SESSION['usuario'] = $usuario['nombre']; // Guardar información del usuario en la sesión
             header("Location: inicio.php"); // Redirigir a la página de inicio
-            exit();
+            exit;
         } else {
             // Contraseña incorrecta
             echo "Contraseña incorrecta";
