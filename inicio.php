4be870c0bc766895969eaf3f1077413073ef4b0d
Josue Ayala <jayalaa1900@alumno.ipn.mx>
2024-02-26 22:26:29 -0600

    Actualizacón: Cambio de funciones MySQL a PostgreSQL en el archivo index.php. Añadidas configuraciones al archivo composer.json esperando asi que lea los archivos .php

--- inicio.php
+++ inicio.php
@@ -1,12 +1,14 @@
 <?php
 
-$servidor = "localhost";
-$usuario = "root";
-$clave = "";
-$baseDeDatos = "loginep";
-
-$enlace = mysqli_connect($servidor, $usuario, $clave, $baseDeDatos);
-
+$servidor = "ec2-52-54-200-216.compute-1.amazonaws.com";
+$usuario = "rzcndrfatvphqy";
+$clave = "1c11fd7412c615db1fa8bc7dd5d5353650f3383ca6f549ee6cf92514cf392ab0";
+$baseDeDatos = "d2em42nge4v4em";
+try{
+    $enlace = pg_connect("pgsql:host= $servidor; dbname=$baseDeDatos user=$usuario password=$clave");
+}catch(PDOException $e){
+    die("Error de conexion: ". $e->getMessage());
+}
 if (!$enlace) {
     echo "Error: No se pudo conectar a MySQL.";
     exit;
@@ -18,16 +20,17 @@ if (isset($_POST["registro"])) {
     $contraseña = $_POST['contraseña'];
 
     // Consulta para verificar si el correo ya existe
-    $consultaCorreo = "SELECT correo FROM Login WHERE correo='$correo'";
-    $resultado = mysqli_query($enlace, $consultaCorreo);
+    $consultaCorreo = "SELECT correo FROM users WHERE correo='$correo'";
+    //$resultado = mysqli_query($enlace, $consultaCorreo);
+    $resultado = pg_query($enlace,$consultaCorreo);
 
     // Verificar si se encontraron resultados (correo ya existe)
-    if (mysqli_num_rows($resultado) > 0) {
+    if (pg_num_rows($resultado) > 0) {
         echo "El correo electrónico ya está registrado. Por favor, utiliza otro correo.";
     } else {
         // El correo no existe, insertar los datos en la base de datos
         $insertarDatos = "INSERT INTO Login VALUES ('$nombre', '$correo', '$contraseña', '')";
-        $ejecutarInsertar = mysqli_query($enlace, $insertarDatos);
+        $ejecutarInsertar = pg_query($enlace, $insertarDatos);
 
         // Verificar si se pudo insertar correctamente
         if ($ejecutarInsertar) {
