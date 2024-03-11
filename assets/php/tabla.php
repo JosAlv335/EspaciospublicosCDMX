6fc990ba63f805c6f58100db46ed79c453bf3b59
D4RKM4773R <jayalaa1900@alumno.ipn.mx>
2024-03-10 18:03:21 -0600

    modificado tabla.php a ver si ahora si jala

--- assets/php/tabla.php
+++ assets/php/tabla.php
@@ -1,22 +1,33 @@
 <?php
-// Recupera datos de Supabase
+// Variables de entorno con las credenciales de Supabase
 $supabaseUrl = getenv("REST_URL");
 $supabaseKey = getenv("REST_PUBLIC_KEY");
+
+// URL de la tabla en Supabase
+$urlTabla = "{$supabaseUrl}/rest/v1/espacios_publicos";
+
+// Configurar las cabeceras de la solicitud HTTP
 $headers = array(
     'Content-Type: application/json',
     'apikey: ' . $supabaseKey,
     'Authorization: Bearer ' . $supabaseKey
 );
 
-// Realiza la solicitud HTTP a Supabase
+// Inicializar cURL
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $supabaseUrl);
+
+// Configurar la solicitud cURL
+curl_setopt($ch, CURLOPT_URL, $urlTabla);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+// Ejecutar la solicitud cURL
 $response = curl_exec($ch);
+
+// Cerrar cURL
 curl_close($ch);
 
-// Devuelve la respuesta al cliente (en formato JSON)
+// Devolver la respuesta al cliente (en formato JSON)
 header('Content-Type: application/json');
 echo $response;
 ?>
