4ba4ff227011343ad220d5acebce101c5f6d3ccf
D4RKM4773R <jayalaa1900@alumno.ipn.mx>
2024-03-10 22:56:28 -0600

    Archivo para obtener los resultados según la busqueda de codigo postal

---
+++ assets/php/buscarAsentamientos.php
@@ -0,0 +1,41 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // URL de la API de Supabase
+    $supabaseUrl = $_ENV["REST_URL"] . "/rest/v1/catalogo_Direcciones";
+    // Clave pública de la API de Supabase
+    $supabaseKey = getenv("REST_PUBLIC_KEY");
+
+    // Recibir el nombre de la búsqueda y eliminar espacios
+    $nombre_busqueda = isset($_GET["codigoPostal"]) ? $_GET["codigoPostal"] : "";
+
+    // Configurar la solicitud HTTP a Supabase
+    $url = $supabaseUrl . '?codigo_postal=like.' . urlencode($nombre_busqueda) . '*';
+    $options = array(
+        'http' => array(
+            'header' => "Content-Type: application/json\r\n" . "apikey: $supabaseKey\r\n",
+            'method' => 'GET'
+        )
+    );
+
+    // Realizar la solicitud HTTP
+    $context = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+
+    // Verificar si la solicitud fue exitosa
+    if ($response === FALSE) {
+        echo "$supabaseUrl - URL";
+        echo "$supabaseKey - Key";
+        echo "$options - options";
+        echo "Error al realizar la solicitud HTTP.";
+    } else {
+        // Decodificar la respuesta JSON
+        $data = json_decode($response, true);
+
+        //Devuelve los resultados en formato JSON
+        echo json_encode($data);
+
+    }
+} else {
+    echo "<h2>No matches found...</h2>";
+}
+?>
