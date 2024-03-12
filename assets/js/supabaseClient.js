b16c93c0a7fac1332d02dec6c02c98a70283b725
D4RKM4773R <jayalaa1900@alumno.ipn.mx>
2024-03-11 19:51:57 -0600

    Ahora si chingaron a su  madre las librerias de Node.js y Supabase

--- assets/js/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// supabaseClient.js
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'tu-url-de-supabase';
-const supabaseAnonKey = 'tu-anon-key';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
