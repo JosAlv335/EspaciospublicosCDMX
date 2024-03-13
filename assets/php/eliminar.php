bb56678f761fe8723a68fd40ec58603c1b4b0be3
D4RKM4773R <jayalaa1900@alumno.ipn.mx>
2024-03-13 00:41:30 -0600

    Añadida función de eliminación (Por probar)

---
+++ assets/php/eliminar.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    //Se obtiene el ID del registro a eliminar
+    $idRegistroAEliminar = $_POST['id']; 
+    $supabaseUrl = $_ENV["REST_URL"] . "/rest/v1/espacios_publicos?id=eq." . $idRegistroAEliminar;
+
+    // Clave pública de la API de Supabase
+    $supabaseKey = getenv("REST_PUBLIC_KEY");
+
+    // Configurar la solicitud HTTP a Supabase para eliminar datos
+    $options = array(
+        'http' => array(
+            'header' => "Content-Type: application/json\r\n" .
+                        "apikey: $supabaseKey\r\n",
+            'method' => 'DELETE',
+        ),
+    );
+
+    // Realizar la solicitud HTTP
+    $context = stream_context_create($options);
+    $response = file_get_contents($supabaseUrl, false, $context);
+
+    // Verificar si la solicitud fue exitosa
+    if ($response === FALSE) {
+        echo "Error al realizar la solicitud HTTP.";
+    } else {
+        // La respuesta incluye los detalles de la operación de eliminación, la mostramos (o procesamos según sea necesario)
+        echo $response;
+    }
+} else {
+    echo "<h2>Método de solicitud no soportado.</h2>";
+}
+?>
