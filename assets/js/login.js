a5adbf725ade1bc087c2829f9239e02e8caf3623
Josue Ayala <jayalaa1900@alumno.ipn.mx>
2024-03-23 00:23:29 -0600

    Adaptacio nes para usar las librerías de la API de SupaBase en lugar de PHP (Login)

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,36 @@
-const btnSignIn = document.getElementById("sign-in");
-      btnSignUp = document.getElementById("sign-up");
-      formRegister = document.querySelector(".register");
-      formLogin = document.querySelector(".login");
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.REST_URL, process.env.REST_PUBLIC_KEY)
 
 
-btnSignIn.addEventListener("click", e => {
-    formRegister.classList.add("hide");
-    formLogin.classList.remove("hide");
-})
 
-btnSignUp.addEventListener("click", e => {
-    formLogin.classList.add("hide");
-    formRegister.classList.remove("hide");
-})
+// Ejemplo de cómo podrías capturar el evento de submit de un formulario
+document.getElementById('miFormularioDeInicioDeSesion').addEventListener('submit', function(e) {
+    e.preventDefault() // Evita el envío tradicional del formulario
+  
+    // Obtiene los valores del formulario
+    const email = document.getElementById('sign-in-mail').value
+    const password = document.getElementById('sign-in-password').value
+  
+    // Llama a la función de inicio de sesión
+    signIn(email, password)
+  })
+  
+
+async function signIn(email, password) {
+    const { user, session, error } = await supabase.auth.signIn({
+      email: email,
+      password: password,
+    })
+  
+    if (error) {
+        console.error('Error en el inicio de sesión:', error.message);
+        document.getElementById('sign-in-error').innerText = error.message;
+
+    } else {
+        console.log('Inicio de sesión exitoso', user, session)
+        // Aquí puedes redirigir al usuario a otra página o manejar la sesión como prefieras
+        window.location.href = '../../paginas/tabla.html';
+    }
+  }
+  
